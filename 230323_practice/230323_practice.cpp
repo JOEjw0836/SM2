873fae535e0700d3e61101ce6aa041c12f235e93
JOEjw0836 <woni7544@naver.com>
2023-03-24 20:45:24 +0900

    practice: 230323_if

--- 230323_practice/230323_practice.cpp
+++ 230323_practice/230323_practice.cpp
@@ -1,10 +1,118 @@
 ﻿#include <iostream>
 
 int main() {
-	std::string str1 = "안녕하세요!";
-	std::string str2 = "반갑습니다.";
+	std::string str1 = "------실습1-------";
+	std::string str2 = "나이를 입력하세요.";
+	int age;
 
 	std::cout << str1;
 	std::cout << "\n";
 	std::cout << str2;
+	std::cout << "\n";
+	std::cin >> age;
+	
+	if (age >= 1 && age <= 7) {
+		std::cout << "유아";
+	}
+	else if (age <= 13) {
+		std::cout << "초등학생";
+	}
+	else if (age <= 16) {
+		std::cout << "중학생";
+	}
+	else if (age <= 19) {
+		std::cout << "고등학생";
+	}
+	else if (age >= 20 && age < 200) {
+		std::cout << "성인";
+	}
+	else {
+		std::cout << "나이가 너무 많습니다..!";
+	}
+
+	std::string str3 = "------실습2------";
+	std::string str4 = "이름을 입력하세요.";
+	std::string name;
+
+	std::cout << "\n";
+	std::cout << str3;
+	std::cout << "\n";
+	std::cout << str4;
+	std::cout << "\n";
+
+	std::cin >> name;
+
+	if (name == "홍길동") {
+		std::cout << "남자";
+	}
+	else if (name == "성춘향") {
+		std::cout << "여자";
+	}
+	else {
+		std::cout << "모르겠어요.";
+	}
+
+	std::string str5 = "------실습3------";
+	std::string str6 = "숫자를 입력하세요.";
+	int num;
+
+	std::cout << "\n";
+	std::cout << str5;
+	std::cout << "\n";
+	std::cout << str6;
+
+	std::cin >> num;
+
+	num % 5 == 0 ? std::cout << num << "는 5의 배수" : std::cout << num << "는 5의 배수가 아님";
+
+	std::string str7 = "------실습4-------";
+	std::string str8 = "연산할 정수 두 개를 입력하시오";
+	std::string str9 = "연산자를 입력하시오";
+	std::string str10 = "------";
+	
+	
+
+	int num1;
+	int num2;
+	char calc;
+
+	std::cout << "\n";
+	std::cout << str7;
+	std::cout << "\n";
+	std::cout << str8;
+	std::cout << "\n";
+	std::cin >> num1;
+	std::cin >> num2;
+	std::cout << "\n";
+	std::cout << str9;
+	std::cout << "\n";
+	std::cin >> calc;
+	std::cout << "\n";	
+	std::cout << str10;
+	std::cout << "\n";
+
+	if (calc == '+' ) {
+		std::cout << num1 + num2;
+	}
+	else if (calc == '-') {
+		std::cout << num1 - num2;
+	}
+	else if (calc == '*') {
+		std::cout << num1 * num2;
+	}
+	else if (calc == '/') {
+		std::cout << "몫:" << num1 / num2 << "나머지는" << num1 % num2 << "입니다.";
+	}
+	else {
+		std::cout << "다시 입력하세요";
+	}
+
+
+
+
+
+
+
+
+
 }
---
+++ 230324_for/230324_for.cpp
@@ -0,0 +1,259 @@
+﻿// 230324_for.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+/*#include <iostream>
+
+int main(){
+    std::cout << "시작합니다";
+    std::cout << "\n";
+
+    for (int i = 0; i < 5; i++) {
+        //(초기값; 조건문 ; 증감문)
+        std::cout << "안녕하세요" << std::endl;
+    }
+
+    for (int a = 10; a > 3; a--) {
+        std::cout << "Hello" << std::endl;
+    }
+
+    std::cout << "끝났습니다";
+
+}
+*/
+
+/*#include <iostream>
+
+int main() {
+    //while ( 조건문) { 조건문이 참인 경우 반복적으로 실행하는 명령문}
+    int i = 0;
+    while (i < 5) {
+        std::cout << "안녕하세요." << std::endl;
+        i++;
+    }
+}
+*/
+
+//while문은 보통 언제까지 반복할지 모를 때 사용
+/*#include <iostream>
+
+int main() {
+    int order;
+    while (1) {
+        std::cout << "1~3 중 하나를 입력하세요";
+        std::cin >> order;
+
+        if (order == 1) {
+            std::cout << "아메리카노 주문" << std::endl;
+        }
+        else if (order == 2) {
+            std::cout << "라떼 주문" << std::endl;
+        }
+        else if (order == 3) {
+            break;
+        }
+        else {
+            std::cout << "1~3을 입력하시오" << std::endl;
+        }
+        
+    }
+    std::cout << "종료합니다";
+}
+*/
+
+//do while문 -- 무조건 1번은 실행시킨 다음 조건을 검사함.
+//while문 -- 조건을 먼저 검사하고 실행시킴.
+
+//do { 반복하고 싶은 것
+//   }
+//while(조건문);
+
+
+
+//실습1 : 구구단 만들기
+
+/*#include <iostream>
+
+int main() {
+    std::cout << "구구단 5단" << std::endl;
+    std::cout << "----------" << std::endl;
+
+    for (int i = 1; i < 10; i++) {
+        std::cout << "5 * " << i << "=" << 5 * i << std::endl;
+    }
+
+    std::cout << "1~9단 출력" << std::endl;
+    std::cout << "----------" << std::endl;
+
+    for (int j = 1; j < 10; j++) {
+        std::cout << "----" << j << "단----" << std::endl;
+        for (int k = 1; k < 10; k++) {
+            std::cout << j << "*" << k << " = " << j * k << std::endl;
+        }
+    }
+
+  
+}
+*/
+
+//실습2 1부터 n까지 합 구하기
+
+/*#include <iostream>
+
+int main() {
+    int n;
+    int sum = 0;
+    int num;
+
+    std::cout << "1부터 n까지의 합 구하기" << std::endl << std::endl;
+    std::cout << "숫자(양의 정수)를 입력하세요 : ";
+    std::cin >> n;
+    std::cout << "\n";
+    std::cout << "1부터 " << n << "까지의 합은 : ";
+
+    for (int num = 1; num <= n;  num++) {
+        sum = sum + num;
+    }
+    std::cout << sum;
+
+
+}
+*/
+
+
+//실습3 while문 사용해보기
+
+/*#include <iostream>
+
+int main() {
+    std::cout << "구구단 5단" << std::endl;
+    std::cout << "----------" << std::endl;
+
+    int i = 1;
+    int j = 1;
+    while (i < 10) {
+        i++;
+        std::cout << "5 * " << i << "=" << 5 * i << std::endl;
+    }
+
+    std::cout << "1~9단 출력" << std::endl;
+    std::cout << "----------" << std::endl;
+
+    while (j < 10) {
+        j++;
+        std::cout << "----" << j << "단----" << std::endl;
+        for (int k = 1; k < 10; k++) {
+            std::cout << j << "*" << k << " = " << j * k << std::endl;
+        }
+    }
+}
+*/
+
+/*#include <iostream>
+
+int main() {
+    int n;
+    int sum = 0;
+    int num = 1;
+
+    std::cout << "1부터 n까지의 합 구하기" << std::endl << std::endl;
+    std::cout << "숫자(양의 정수)를 입력하세요 : ";
+    std::cin >> n;
+    std::cout << "\n";
+    std::cout << "1부터 " << n << "까지의 합은 : ";
+
+    while(num <= n) {
+       
+        sum = sum + num;
+        num++;
+    }
+    std::cout << sum;
+
+
+}*/
+
+/*#include <iostream>
+void multiple(int num1, int num2) {
+    std::cout << num1 * num2;
+   
+}
+int main() {
+    multiple(3,6);
+}
+*/
+
+//실습4: 사용자가 입력한 숫자 더하기
+
+/*#include <iostream>
+
+int main() {
+    int num;
+    int sum = 0;
+    std::cout << "사용자가 입력한 숫자 더하기";
+    std::cout << "\n";
+
+    do
+    {
+        std::cout << "숫자를 입력하세요. (0, exit) : ";
+        std::cin >> num;
+        sum += num;
+    } while (num);
+    std::cout << "사용자가 입력한 수의 합은 : " << sum;
+}
+*/
+
+/*#include <iostream>
+
+int add(int n1, int n2) {
+    int result = n1 + n2;
+
+    return result;
+}
+
+int minus(int n1, int n2) {
+    if (n1 > n2) {
+        int result1 = n1 - n2;
+
+        return result1;
+    }
+    else if (n2 > n1) {
+        int result2 = n2 - n1;
+
+        return result2;
+    }
+}
+
+int multiple(int n1, int n2) {
+    int result = n1 * n2;
+
+    return result;
+}
+
+double divide(int n1, int n2) {
+    double result = (double)n1 / (double)n2;
+
+    return result;
+}
+
+int main() {
+    std::cout << add(4, 3) << std::endl;
+    std::cout << minus(4, 3) << std::endl;
+    std::cout << multiple(4, 3) << std::endl;
+    std::cout << divide(4, 3) << std::endl;
+}
+*/
+
+#include <iostream>
+
+void test(int num) {
+
+    if (num % 2 == 1) {
+        std::cout << "홀수";
+    }
+    else if (num % 2 == 0) {
+        std::cout << "짝수";
+    }
+}
+
+int main() {
+    test(5);
+}
